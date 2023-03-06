dcc52bd391efbeccf2db4c5cb035d84455c428ba
kawax <kawaxbiz@gmail.com>
2023-03-06 15:34:09 +0900

    Update WordWrap.php

--- src/Str/WordWrap.php
+++ src/Str/WordWrap.php
@@ -6,8 +6,8 @@ namespace Revolution\Laravel\Mixins\Str;
 
 class WordWrap
 {
-    public function __invoke(string $str, int $width = 10, string $break = PHP_EOL): string
+    public function __invoke(?string $str, int $width = 10, string $break = PHP_EOL): string
     {
-        return implode($break, mb_str_split($str, $width));
+        return implode($break, mb_str_split($str ?? '', $width));
     }
 }
