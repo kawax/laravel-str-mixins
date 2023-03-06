ab157cd9e93bbc54561a6919e3fcb8adf1821923
kawax <kawaxbiz@gmail.com>
2023-03-06 15:34:06 +0900

    Update Kana.php

--- src/Str/Kana.php
+++ src/Str/Kana.php
@@ -6,8 +6,8 @@ namespace Revolution\Laravel\Mixins\Str;
 
 class Kana
 {
-    public function __invoke(string $str, string $option = 'KV', string $encoding = 'UTF-8'): string
+    public function __invoke(?string $str, string $option = 'KV', string $encoding = 'UTF-8'): string
     {
-        return mb_convert_kana($str, $option, $encoding);
+        return mb_convert_kana($str ?? '', $option, $encoding);
     }
 }
