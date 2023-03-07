b44d33d96da3f6f65de27fa5a762e15b02f88b39
kawax <kawaxbiz@gmail.com>
2023-03-07 09:56:33 +0900

    Update StrMixinsServiceProvider.php

--- src/StrMixinsServiceProvider.php
+++ src/StrMixinsServiceProvider.php
@@ -7,16 +7,18 @@ namespace Revolution\Laravel\Mixins;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Illuminate\Support\Stringable;
+use ReflectionException;
 use Revolution\Laravel\Mixins\Str\Kana;
 use Revolution\Laravel\Mixins\Str\Truncate;
 use Revolution\Laravel\Mixins\Str\WordWrap;
+use Revolution\Laravel\Mixins\Stringable\Japanese;
 
 class StrMixinsServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap services.
      *
-     * @return void
+     * @throws ReflectionException
      */
     public function boot(): void
     {
@@ -24,26 +26,6 @@ class StrMixinsServiceProvider extends ServiceProvider
         Str::macro('kana', new Kana());
         Str::macro('truncate', new Truncate());
 
-        $this->fluent();
-    }
-
-    protected function fluent(): void
-    {
-        Stringable::macro('wordwrap', function (int $width = 10, string $break = PHP_EOL): static {
-            /** @var Stringable $this */
-            return new static(Str::wordwrap($this->value ?? '', $width,
-                $break));
-        });
-
-        Stringable::macro('kana', function (string $option = 'KV', string $encoding = 'UTF-8'): static {
-            /** @var Stringable $this */
-            return new static(Str::kana($this->value ?? '', $option,
-                $encoding));
-        });
-
-        Stringable::macro('truncate', function (int $limit = 100, string $end = '...'): static {
-            /** @var Stringable $this */
-            return new static(Str::truncate($this->value ?? '', $limit, $end));
-        });
+        Stringable::mixin(new Japanese());
     }
 }
